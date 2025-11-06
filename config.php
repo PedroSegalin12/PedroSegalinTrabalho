1b55c0a77c4897eddd32eee426ad25b8ddc21b8f
PedroSegalin12 <pedro.segalin@alunos.sc.senac.br>
2025-11-05 22:14:19 -0300

    05/11

--- config.php
+++ config.php
@@ -1,9 +1,28 @@
 <?php
+class Conexao {
 
-define('DB_HOST', 'sql102.byetcluster.com');
-define('DB_USER', 'if0_40232507');
-define('DB_PASS', 'PedroSega2002');
-define('DB_NAME', 'if0_40232507_landing_page');
 
+	private $host = 'sql102.byetcluster.com';
+	private $dbname = 'if0_40232507_landing_page';
+	private $user = 'if0_40232507';
+	private $pass = 'PedroSega2002';
+
+	public function conectar() {
+		try {
+
+			$conexao = new PDO(
+				"mysql:host=$this->host;dbname=$this->dbname",
+				"$this->user",
+				"$this->pass"				
+			);
+
+			return $conexao;
+
+
+		} catch (PDOException $e) {
+			echo '<p>'.$e->getMessege().'</p>';
+		}
+	}
+}
 
 ?>
--- enviarbanco.php
+++ enviarbanco.php
@@ -1,18 +1,47 @@
 <?php
+require_once 'config.php';
+
+$sql_insert = "INSERT INTO mensagens (nome, email, mensagem, data_envio) VALUES (:nome, :email, :mensagem, NOW())";
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
     $nome = $_POST['nome'] ?? '';
     $email = $_POST['email'] ?? '';
     $mensagem = $_POST['mensagem'] ?? '';
 
     if ($nome && $email && $mensagem) {
-        echo "<h2>Mensagem enviada com sucesso!</h2>";
-        echo "<p><strong>Nome:</strong> " . htmlspecialchars($nome) . "</p>";
-        echo "<p><strong>E-mail:</strong> " . htmlspecialchars($email) . "</p>";
-        echo "<p><strong>Mensagem:</strong> " . nl2br(htmlspecialchars($mensagem)) . "</p>";
+        
+        try {
+            $conexaoObj = new Conexao(); 
+            $conn = $conexaoObj->conectar(); 
+
+            $stmt = $conn->prepare($sql_insert);
+            
+            $stmt->bindParam(':nome', $nome);
+            $stmt->bindParam(':email', $email);
+            $stmt->bindParam(':mensagem', $mensagem);
+            
+            if ($stmt->execute()) {
+                echo "<h2>Mensagem enviada com sucesso!</h2>";
+                echo "<p>Os dados foram salvos no banco de dados.</p>";
+                echo "<p><strong>Nome:</strong> " . htmlspecialchars($nome) . "</p>";
+                echo "<p><strong>E-mail:</strong> " . htmlspecialchars($email) . "</p>";
+                echo "<p><strong>Mensagem:</strong> " . nl2br(htmlspecialchars($mensagem)) . "</p>";
+            } else {
+                echo "<h2>Erro ao salvar no Banco de Dados!</h2>";
+                echo "<p>Detalhes do Erro: " . $stmt->errorInfo()[2] . "</p>"; 
+            }
+            
+            $stmt->closeCursor(); 
+
+        } catch (\PDOException $e) {
+            die("Falha Crítica na Conexão/PDO (Verifique o config.php): " . $e->getMessage());
+        }
+
     } else {
-        echo "Preencha todos os campos!";
+        echo "<h2>Preencha todos os campos!</h2>";
     }
 } else {
-    echo "Método inválido!"; // importante para testar no navegador
+    echo "Método inválido!";
 }
 ?>
